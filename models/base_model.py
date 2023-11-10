33b66643e2f8442ce22e54dd4377996f7cad779f
marsade <adenijifara@gmail.com>
2023-11-10 23:49:26 +0200

    add to_dict() method and save() method

--- models/base_model.py
+++ models/base_model.py
@@ -1,28 +1,34 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 """This module contains the base class for all classes"""
 import datetime
-from typing import Any
 import uuid
 
 class BaseModel:
     """Base class for all models
       
     Attributes:
-    id(str): string containing instance uuid
-       	created_at(datetime): current datetime when an instance is created
-        	updated_at(datetime): current datetime and updated every time object is changed
-      """
-    id = str(uuid.uuid4)
-    created_at = datetime.datetime.now()
-    updated_at = datetime.datetime.now()
+        id(str): string containing instance uuid
+        created_at(datetime): current datetime when an instance is created
+        updated_at(datetime): current datetime and updated every time object is changed
+    """
+    def __init__(self):
+        """Initialize the class instance"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
 
     def save(self):
         """updates updated_at information"""
         self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
-        pass
+        """returns a dict representation of the Base object"""
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
 
     def __str__(self):
-            return f"{self.__class__.__name__} {self.id} {self.__dict__}"
-
+        """Returns a string representation of the object"""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
