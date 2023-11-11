7e70a2441397b24b9fdee34f4984e82d450ff2e4
marsade <adenijifara@gmail.com>
2023-11-11 11:16:01 +0200

    Add tests folder

---
+++ tests/test_models/test_basemodel.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Unit tests for the Base class"""
+import datetime
+from models.base_model import BaseModel
+import unittest
+
+
+class TestBaseModel(unittest.TestCase):
+      def setUp(self):
+            self.bm1 = BaseModel()
+
+      def tearDown(self):
+            self.bm1.cleanup()
+
+      def test_created_at(self):
+            self.assertTrue(hasattr(self.bm1, 'created_at'))
+            self.assertIsInstance(self.bm1.created_at, datetime.datetime)
+
+      def test_uuid(self):
+            pass
+      
