3455d4812b58c422bef328e90076f4e9bb7b0c5a
marsade <adenijifara@gmail.com>
2023-11-13 13:27:53 +0200

    Add file

---
+++ tests/test_console.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Test for the airbnb console"""
+import console
+import unittest
+
+
+class TestConsole(unittest.TestCase):
+    """Tests the console functionality"""
+    
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
