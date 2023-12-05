baccdd6c574f0d9ad3982cb29c4f683f8a987b73
marsade <adenijifara@gmail.com>
2023-12-05 21:42:20 +0200

    Complete task 6

--- console.py
+++ console.py
@@ -4,7 +4,18 @@ import models
 
 
 class HBNBCommand(cmd.Cmd):
-    pass
+    prompt = "(hbnb) "
+
+    def do_EOF(self, line):
+        print("")
+        return True
+
+    def do_quit(self, line):
+        return True
+
+    def emptyline(self):
+        pass
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
