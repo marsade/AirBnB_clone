db281c92bf0b955c958a419b8b3f64bb6b978d3b
marsade <adenijifara@gmail.com>
2023-12-06 19:39:57 +0200

    Add updated_at attribute

--- models/base_model.py
+++ models/base_model.py
@@ -19,6 +19,7 @@ class BaseModel:
         """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
+        self.updated_at = datetime.now()
         if len(kwargs) != 0:
             for key, value in kwargs.items():
                 if key == '__class__':
