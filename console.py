052cc80f6a381c2f5567cc54e86edf79e6d4f433
Fara <97781374+marsade@users.noreply.github.com>
2023-12-06 10:39:57 +0000

    Implement create command in interpreter

--- console.py
+++ console.py
@@ -6,17 +6,29 @@ import models
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
+    __classes = [
+        "BaseModel"
+    ]
 
     def do_EOF(self, line):
         print("")
         return True
 
     def do_quit(self, line):
+        """Quits from the console"""
         return True
 
     def emptyline(self):
+        """Does nothing when an empty line is passed"""
         pass
 
-
+    def do_create(self, line):
+        class_name = line.split()[0]
+        if len(line) == 0:
+            print("** class name missing **")
+        elif class_name not in self.__classes:
+            print("** class doesn't exist **")
+        else:
+            pass
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
